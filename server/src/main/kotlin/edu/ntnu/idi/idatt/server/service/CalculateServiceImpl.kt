7a4c5d606010bc34886729c495eba9d12e3b417b
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-02-28 13:25:20 +0100

    Calculate service now works

--- server/src/main/kotlin/edu/ntnu/idi/idatt/server/service/CalculateServiceImpl.kt
+++ server/src/main/kotlin/edu/ntnu/idi/idatt/server/service/CalculateServiceImpl.kt
@@ -5,11 +5,43 @@ import org.springframework.stereotype.Service
 @Service
 class CalculateServiceImpl : CalculateService {
     override fun calculate(input: String): Double {
-        val cleanInput = input.trim().replace("\\s+".toRegex(), "")
-        return 0.1
+        val cleanInput = input.replace("\\s+".toRegex(), "").replace("\"", "");
+        return evaluateExpression(cleanInput);
     }
 
     private fun evaluateExpression(expression: String): Double {
-        return 0.0;
+        var processedExpression = expression;
+
+        val mulDivRegex = "(-?\\d+\\.?\\d*)([*/])(-?\\d+\\.?\\d*)".toRegex()
+        while (mulDivRegex.containsMatchIn(processedExpression)) {
+            processedExpression = mulDivRegex.replace(processedExpression) { matchResult ->
+                val (a, op, b) = matchResult.destructured
+                val result = when (op) {
+                    "*" -> a.toDouble() * b.toDouble()
+                    "/" -> a.toDouble() / b.toDouble()
+                    else -> 0.0
+                }
+                result.toString()
+            }
+        }
+
+        val addSubRegex = "(-?\\d+\\.?\\d*)([+\\-])(\\d+\\.?\\d*)".toRegex()
+        while (addSubRegex.containsMatchIn(processedExpression)) {
+            processedExpression = addSubRegex.replace(processedExpression) { matchResult ->
+                val (a, op, b) = matchResult.destructured
+                val result = when (op) {
+                    "+" -> a.toDouble() + b.toDouble()
+                    "-" -> a.toDouble() - b.toDouble()
+                    else -> 0.0
+                }
+                result.toString()
+            }
+        }
+
+        return try {
+            processedExpression.toDouble()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("Invalid expression: $expression")
+        }
     }
 }
