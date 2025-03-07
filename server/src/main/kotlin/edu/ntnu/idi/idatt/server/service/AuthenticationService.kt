1f6ec60e0e683ce9bc5b75bc8cb9938afef36609
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-03-07 13:39:39 +0100

    WIP: Adding endpoints for authentication

--- server/src/main/kotlin/edu/ntnu/idi/idatt/server/service/AuthenticationService.kt
+++ server/src/main/kotlin/edu/ntnu/idi/idatt/server/service/AuthenticationService.kt
@@ -2,7 +2,7 @@ package edu.ntnu.idi.idatt.server.service
 
 import edu.ntnu.idi.idatt.server.model.User
 
-interface UserService {
-    fun createUser(username: String, email: String, password: String): User
-    fun login(username: String, password: String): User?
+interface AuthenticationService {
+    fun signup(username: String, email: String, password: String): User
+    fun authenticate(username: String, password: String): User?
 }
