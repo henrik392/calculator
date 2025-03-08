4ae6f1a7f9c16edbba7d823931f5431360bfe1bb
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-03-08 14:36:44 +0100

    Removed all instances of user_id

--- server/src/main/kotlin/edu/ntnu/idi/idatt/server/repository/UserRepository.kt
+++ server/src/main/kotlin/edu/ntnu/idi/idatt/server/repository/UserRepository.kt
@@ -5,7 +5,5 @@ import edu.ntnu.idi.idatt.server.model.User
 interface UserRepository {
     fun save(user: User): User
     fun existsByUsername(username: String): Boolean
-    fun findByUserId(userId: Long): User?
     fun findByUsername(username: String): User?
-    fun login(username: String, password: String): User?
 }
