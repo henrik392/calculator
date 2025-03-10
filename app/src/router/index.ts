c442e19b665e4543b784e54e1949b8f5793eb97a
Henrik Kvamme <henrik.halvorsen.kvamme@gmail.com>
2025-03-10 15:27:46 +0100

    WIP: Auth page

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -2,17 +2,34 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../HomePage.vue';
 import ContactPage from '../ContactPage.vue';
 import LoginPage from '../LoginPage.vue';
+import { useAuthStore } from '@/stores/auth';
+import { esbuildVersion } from 'vite';
 
 const routes = [
-    { path: '/', component: HomePage, name: 'home' },
-    { path: '/home', redirect: '/' },
-    { path: '/login', component: LoginPage, name: 'login' },
+    {
+        path: '/',
+        component: HomePage,
+        name: 'home',
+        // meta: { requiresAuth: true },
+    },
+    // { path: '/login', component: LoginPage, name: 'login' },
+    // { path: '/signup', component: LoginPage, name: 'login' },
     { path: '/contact', component: ContactPage, name: 'contact' },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+});
+
+router.beforeEach((to, _from, next) => {
+    const authStore = useAuthStore();
+
+    if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+        next('/login');
+    } else {
+        next();
+    }
 });
 
 export default router;
